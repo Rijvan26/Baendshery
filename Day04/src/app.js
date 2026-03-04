c41e00804be9147cc7cf2e38d9cfa92f9563edfb
Rijvaan <khanrijvan2610@gmail.com>
2026-03-03 17:04:41 -0800

    Day04 postman 4  oprations done

---
+++ Day04/src/app.js
@@ -0,0 +1,40 @@
+// kaam 1 server ko create krna
+// 2. server ko config krna
+
+const express = require("express")
+
+const app = express()
+
+const notes = []
+app.use(express.json())
+
+
+app.get("/", (req,res) => {
+    res.send("hello world")
+})
+
+app.post("/notes", (req,res) => {
+    console.log(req.body);
+    notes.push(req.body)
+
+    console.log(notes);
+
+    res.send("notes created")  
+})
+
+app.get("/notes",(req,res) => {
+    res.send(notes)
+})
+
+app.delete("/notes/:index", (req,res) => {
+      delete notes [req.params.index]
+      res.send("note deleted succesfully")
+})
+
+// patch /notes/:index
+
+app.patch("/notes/:index", (req,res) => {
+    notes[req.params.index].description = req.body.description
+    notes[req.params.index].title = req.body.title
+})
+module.exports = app
