ea028215edfe4dd0f02873a82f556c06c39e2949
Rijvaan <khanrijvan2610@gmail.com>
2026-05-06 23:03:15 +0530

    fix cors error in forntend

--- InstaClone1/Backend/server.js
+++ InstaClone1/Backend/server.js
@@ -3,11 +3,6 @@ const connectToDb = require("./src/config/database")
 const mongoose = require("mongoose")
 const dns = require("node:dns")
 dns.setServers(['1.1.1.1', '8.8.8.8'])
-const cors = require("cors")
-app.use(cors({
-    origin: "https://rizzzgram.onrender.com",
-    credentials: true,
-}))
 
 
 
