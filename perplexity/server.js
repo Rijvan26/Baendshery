253c4bc487eb2800dcd75504ecd6a06f685381bb
Rijvaan <khanrijvan2610@gmail.com>
2026-05-28 22:55:17 +0530

    gemin api intregrated

--- perplexity/server.js
+++ perplexity/server.js
@@ -2,10 +2,12 @@ import dotenv from "dotenv"
 import app from "./src/app.js"
 import dns from "node:dns"
 import connectToDb from "./src/config/database.js"
+import {testAi} from "./src/services/ai.service.js"
     dotenv.config()
 dns.setServers(['1.1.1.1', '8.8.8.8'])
 
 connectToDb()
+testAi()
 
 app.listen(3000, () => {
     console.log("server is running on port 3000")
