25fe9ba173c1f0d06283ac89db90fea9de75f8b2
Rijvaan <khanrijvan2610@gmail.com>
2026-06-08 17:08:26 +0530

    node mailer issue

--- Perplexityy/Backend/src/services/mail.service.js
+++ Perplexityy/Backend/src/services/mail.service.js
@@ -2,23 +2,18 @@ import dotenv from "dotenv"
 dotenv.config()
 import nodemailer from "nodemailer";
 
-
-console.log("GOOGLE_USER:", process.env.GOOGLE_USER);
-console.log("CLIENT_ID exists:", !!process.env.GOOGLE_CLIENT_ID);
-console.log("CLIENT_SECRET exists:", !!process.env.GOOGLE_CLIENT_SECRET);
-console.log("REFRESH_TOKEN exists:", !!process.env.GOOGLE_REFRESH_TOKEN);
-
-
 const transporter = nodemailer.createTransport({
-    service:"gmail",
-    auth:{ 
-      type:"oauth2",
-        user: process.env.GOOGLE_USER,
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken: process.env.GOOGLE_REFRESH_TOKEN
-    }
-})
+  service: "gmail",
+  auth: {
+    type: "oauth2",
+    user: process.env.GOOGLE_USER,
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+  },
+  logger: true,
+  debug: true,
+});
 
 
 
