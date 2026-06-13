2dd14b2d04a5b9bfce6273ac5f097bcd1e02e767
Rijvaan <khanrijvan2610@gmail.com>
2026-06-14 01:52:25 +0530

    isuue email

--- Perplexityy/Backend/src/services/mail.service.js
+++ Perplexityy/Backend/src/services/mail.service.js
@@ -1,27 +1,26 @@
-import * as SibApiV3Sdk from "@getbrevo/brevo";
+import * as brevo from "@getbrevo/brevo";
+console.log(Object.keys(brevo));
 
-const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+const apiInstance = new brevo.TransactionalEmailsApi();
 
 apiInstance.setApiKey(
-  SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey,
+  brevo.TransactionalEmailsApiApiKeys.apiKey,
   process.env.BREVO_API_KEY
 );
 
-console.log("BREVO SDK:", Object.keys(SibApiV3Sdk));
-
-console.log("BREVO API KEY EXISTS:", !!process.env.BREVO_API_KEY);
-
+console.log("BREVO_API_KEY:", !!process.env.BREVO_API_KEY);
 export async function sendEmail({ to, subject, html }) {
   try {
-    const sendSmtpEmail = {
-      sender: {
-        name: "Perplexity",
-        email: "khanrijvan2610@gmail.com",
-      },
-      to: [{ email: to }],
-      subject,
-      htmlContent: html,
+    const sendSmtpEmail = new brevo.SendSmtpEmail();
+
+    sendSmtpEmail.sender = {
+      name: "Perplexity",
+      email: "khanrijvan2610@gmail.com",
     };
+
+    sendSmtpEmail.to = [{ email: to }];
+    sendSmtpEmail.subject = subject;
+    sendSmtpEmail.htmlContent = html;
 
     const result = await apiInstance.sendTransacEmail(sendSmtpEmail);
 
